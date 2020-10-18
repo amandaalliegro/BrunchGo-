a16e494ff37b5c779e4579863923fef62df12644
erikleisinger <erikleisinger@gmail.com>
2020-10-18 12:56:16 -0400

    added cookie-session as a dependency and altered server.js so users are assigned a uniqe id upon reaching the homepage

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -30,6 +31,10 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
+app.use(cookieSession ({
+  name: 'session',
+  keys: ['a']
+}));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -47,7 +52,8 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-
+  const newId = Math.round(Math.random() * 100000);
+  req.session.user_id = newId;
   res.render("index");
 
 });
