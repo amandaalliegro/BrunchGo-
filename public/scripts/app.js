4f9041d939f0fb7834463c216c6e6629b8858e68
erikleisinger <erikleisinger@gmail.com>
2020-10-18 11:47:23 -0400

    restructured the three ajax requests in app.js so they run one after the other, instead of at the same time. This fixed some issues where the appetizers/mains/desserts menus would load at different speeds

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,74 +31,81 @@ $(() => {
 
             $('#main-container > .row:first-child').append(menuItem);
     }
-  });
+  }).then(() => {
 
-  // RENDER "MAINS" MENU ROW
+// RENDER "MAINS" MENU ROW
 
-  $.ajax({
-    method: "GET",
-    url: "/api/menu/mains"
-  }).then((items) => {
+$.ajax({
+  method: "GET",
+  url: "/api/menu/mains"
+}).then((items) => {
 
-    createLocalDatabase(items);
-
-
-
-    let newMenuCategory = `<div class="row">
-    <a id="mains"></a>
-    <h2>Mains</h2>
-    </div>`;
-
-    $('#main-container').append(newMenuCategory);
-
-  for (let item of items) {
-    let menuItem = `
-    <div class="d-flex justify-content-center col-md-4 text-center">
-    <a href="#" class="thumbnail">
-      <img src="//placehold.it/200" alt="Card image cap">
-    </a>
-    <h2>${item.name}</h2>
-    <span><a class="btn btn-default" href="#" role="button">-</a></span>
-    <span id="item1">0</span>
-    <span><a class="btn btn-default" href="#" role="button">+</a></span>
-  </div>
-          `
-
-    $('#main-container > .row:nth-child(2)').append(menuItem);
-  }
-  });
-
-  // // RENDER "DESSERTS" MENU ROW
-
-  $.ajax({
-    method: "GET",
-    url: "/api/menu/desserts"
-  }).then((items) => {
-
-    createLocalDatabase(items);
+  createLocalDatabase(items);
 
 
-    let newMenuCategory = `<div class="row">
-    <a id="desserts"></a>
-    <h2>Desserts</h2>
-    </div>`;
 
-    $('#main-container').append(newMenuCategory);
+  let newMenuCategory = `<div class="row">
+  <a id="mains"></a>
+  <h2>Mains</h2>
+  </div>`;
 
-  for (let item of items) {
-    let menuItem = `
-    <div class="d-flex justify-content-center col-md-4 text-center">
-    <a href="#" class="thumbnail">
-      <img src="//placehold.it/200" alt="Card image cap">
-    </a>
-    <h2>${item.name}</h2>
-    <span><a class="btn btn-default" href="#" role="button">-</a></span>
-    <span id="item1">0</span>
-    <span><a class="btn btn-default" href="#" role="button">+</a></span>
-  </div>
-          `
+  $('#main-container').append(newMenuCategory);
 
-    $('#main-container > .row:nth-child(3)').append(menuItem);
-  }
-  });
+for (let item of items) {
+  let menuItem = `
+  <div class="d-flex justify-content-center col-md-4 text-center">
+  <a href="#" class="thumbnail">
+    <img src="//placehold.it/200" alt="Card image cap">
+  </a>
+  <h2>${item.name}</h2>
+  <span><a class="btn btn-default" href="#" role="button">-</a></span>
+  <span id="item1">0</span>
+  <span><a class="btn btn-default" href="#" role="button">+</a></span>
+</div>
+        `
+
+  $('#main-container > .row:nth-child(2)').append(menuItem);
+}
+}).then(() => {
+// // RENDER "DESSERTS" MENU ROW
+
+$.ajax({
+  method: "GET",
+  url: "/api/menu/desserts"
+}).then((items) => {
+
+  createLocalDatabase(items);
+
+
+  let newMenuCategory = `<div class="row">
+  <a id="desserts"></a>
+  <h2>Desserts</h2>
+  </div>`;
+
+  $('#main-container').append(newMenuCategory);
+
+for (let item of items) {
+  let menuItem = `
+  <div class="d-flex justify-content-center col-md-4 text-center">
+  <a href="#" class="thumbnail">
+    <img src="//placehold.it/200" alt="Card image cap">
+  </a>
+  <h2>${item.name}</h2>
+  <span><a class="btn btn-default" href="#" role="button">-</a></span>
+  <span id="item1">0</span>
+  <span><a class="btn btn-default" href="#" role="button">+</a></span>
+</div>
+        `
+
+  $('#main-container > .row:nth-child(3)').append(menuItem);
+}
+});
+
+})
+
+  })
+
+
+
+
 });
