585f3e9d470976620479b0671387d90b7a736ae5
erikleisinger <erikleisinger@gmail.com>
2020-10-20 12:04:17 -0400

    reverted index.ejs and server.js back to a useable state

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 // load .env data into process.env
 require('dotenv').config();
 
-const { sendSMS } = require("./helpers");
-
 // Web server config
 const PORT       = process.env.PORT || 3050;
 const ENV        = process.env.ENV || "development";
@@ -13,13 +11,11 @@ const app        = express();
 const morgan     = require('morgan');
 const cookieSession = require('cookie-session');
 
-
 // PG database client/connection setup
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
-
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -27,10 +23,7 @@ db.connect();
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
-
-// Need this for postman; need to check if it's needed for the app
-app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
@@ -43,21 +36,17 @@ app.use(cookieSession ({
   keys: ['a']
 }));
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1','key2']
-}))
-
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const menuRoutes = require("./routes/menu");
 const widgetsRoutes = require("./routes/widgets");
-const { reapIntervalMillis } = require('pg/lib/defaults');
+const ordersRoutes = require('./routes/orders')
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/menu", menuRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use('api/orders', ordersRoutes(db))
 // Note: mount other resources here, using the same pattern above
 
 
@@ -65,27 +54,13 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-
-  res.send('hello');
-  // if (req.session.user_id) {
-  //   res.render("index");
-  // } else {
-  //   const newId = Math.round(Math.random() * 100000);
-  //   req.session.user_id = newId;
-  //   res.render("index");
-  // }
-});
-
-
-// test the POST method for sending SMS message
-app.post('/test', (req, res) => {
-
-  const { phone, message } = req.body;
-  // console.log(typeof req.body);
-  // const {phone, message} = req.body;
-  sendSMS(phone, message);
-  res.send('SMS message sent');
-
+  if (req.session.user_id) {
+    res.render("index");
+  } else {
+    const newId = Math.round(Math.random() * 100000);
+    req.session.user_id = newId;
+    res.render("index");
+  }
 });
 app.get("/login", (req, res) => {
   if (req.session.user_id) {
