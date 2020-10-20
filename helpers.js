c85304afaf6f031570e571b03772c2172445041c
George Tang <georgecctang@gmail.com>
2020-10-20 14:55:25 +0000

    update routes

--- helpers.js
+++ helpers.js
@@ -5,14 +5,18 @@ const authToken = 'efedbc49a59307b60c44ac002fbe3eb0';
 // Send SMS message with Twilio
 const client = require('twilio')(accountSid, authToken);
 
-const sendSMS = function(phoneNumber, messageBody) {
+const sendSMS = function(phoneNumber, messageBody, res) {
 
   client.messages.create({
     to: `+1${phoneNumber}`,
     from: `+16473721872`,
     body: messageBody
   })
-  .then(message => console.log(message.sid));
+  .then(message => console.log(message.sid))
+  .catch(error => {
+    console.log('***error delivering message***');
+    res.send("cannot deliver message");
+  });
 }
 
 module.exports = { sendSMS };
