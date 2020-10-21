84f34db7bdffb884495340b79ebbf460add21996
George Tang <georgecctang@gmail.com>
2020-10-21 20:39:21 +0000

    fix item_seeds

--- db/seeds/02_items_seeds.sql
+++ db/seeds/02_items_seeds.sql
@@ -40,7 +40,7 @@ INSERT INTO orders (restaurant_id, name, phone, place_order_datetime, sub_total,
 VALUES(100, 'Erik', '3278424', '1000-01-01 00:00:00', 40, 15, 55);
 
 
-INSERT INTO order_items (order_id, item_id, quantity, specifications)
-VALUES(1, 6, 3, NULL),
-(1, 4, 2, NULL),
-(1, 5, 5, NULL);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (1, 6, 3),
+(1, 4, 2),
+(1, 5, 5);
